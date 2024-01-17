aa14b4869944b429af3ac44f8d817c0783730c78
Shiva <qshiva906@gmail.com>
2024-01-17 21:28:28 +0545

    making notes in javascript.

---
+++ javascript/practise.js
@@ -0,0 +1,20 @@
+// Challenge #4
+
+const variable = 20;
+const variable1 = '30';
+
+const resultVariable = variable + variable1;
+
+console.log(resultVariable);
+// ans: 2030
+typeof(resultVariable);
+// ans: string
+
+// challenge 5
+const objectVariable = {
+    property1: 'i am property 1', property2: 'i am property 2', property3: [20, 30, 40]
+  };
+  undefined
+  console.log(objectVariable.property3[2]);
+//   ans: 40
+
